6e7ad0b4bb7c09c23f97bf3e1ebbbabb07e005aa
therealgabryx <gabriele.concli@gmail.com>
2020-10-31 14:34:16 +0000

    removed unused CRA-template files and basic sections layouted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Components
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Solutions from './sections/Solutions'
+import Services from './sections/Services'
+import CallToAction from './sections/CallToAction'
+import Footer from './sections/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero/>
+      <About/>
+      <Solutions/>
+      <Services/>
+      <CallToAction/>
+      <Footer/>
     </div>
   );
 }
